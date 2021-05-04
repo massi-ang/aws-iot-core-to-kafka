7e0eb9c7349c8dddf422b4bcffc1a8efa1ea7fc4
Massimiliano Angelino <angmas@amazon.com>
2021-05-04 16:06:20 +0200

    initial

--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as LambdaToKafka from '../lib/lambda-to-kafka-stack';
+import * as Cdk from '../lib/cdk-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new LambdaToKafka.LambdaToKafkaStack(app, 'MyTestStack');
+    const stack = new Cdk.CdkStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
